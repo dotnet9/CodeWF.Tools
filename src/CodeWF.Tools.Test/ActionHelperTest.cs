fce64a08c8a3e3882ed4088b41a6f94b0c099c1e
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-07-18 16:16:02 +0800

    add action check helper;

---
+++ src/CodeWF.Tools.Test/ActionHelperTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using CodeWF.Tools.Extensions;
+using CodeWF.Tools.Helpers;
+
+namespace CodeWF.Tools.Test;
+
+public class ActionHelperTest
+{
+    [Fact]
+    public void Test_CheckNormalAction_Success()
+    {
+        var check = ActionHelper.CheckOvertime(DoSomething, 400);
+        Assert.True(check);
+        return;
+
+        void DoSomething()
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(300));
+        }
+    }
+
+    [Fact]
+    public void Test_CheckOvertimeAction_Success()
+    {
+        var cancellationTokenSource = new CancellationTokenSource();
+        var check = ActionHelper.CheckOvertime(DoSomething, 3000);
+        Debug.WriteLine("Cancel running");
+        cancellationTokenSource.Cancel();
+        Assert.False(check);
+        Debug.WriteLine("Check over");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+        Debug.WriteLine("End Test");
+        return;
+
+        void DoSomething()
+        {
+            var beginTime = DateTime.Now;
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                Debug.WriteLine($"{beginTime.GetDiffTime(DateTime.Now)}: Test overtime");
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+        }
+    }
+}
