eb272757b1ec87d91de8b85a7678da2437c9859a
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-07-30 10:18:41 +0800

    [update]fix DateTimeConverter;

--- src/CodeWF.Tools.Test/Tools/DateTimeConverterTest.cs
+++ src/CodeWF.Tools.Test/Tools/DateTimeConverterTest.cs
@@ -3,9 +3,23 @@ namespace CodeWF.Tools.Tests;
 
 public class DateTimeConverterTest
 {
-    [Fact]
+    [Fact(DisplayName = "should return true for valid ISO 8601 date strings")]
     public void ShouldReturnTrueForValidISO8601DateStringgs()
     {
         Assert.True(DateTimeConverter.IsISO8601DateTimeString("2021-01-01T00:00:00.000Z"));
+        Assert.True(DateTimeConverter.IsISO8601DateTimeString("2023-04-12T14:56:00+01:00"));
+        Assert.True(DateTimeConverter.IsISO8601DateTimeString("20230412T145600+0100"));
+        Assert.True(DateTimeConverter.IsISO8601DateTimeString("20230412T145600Z"));
+        Assert.True(DateTimeConverter.IsISO8601DateTimeString("2016-02-01"));
+        Assert.True(DateTimeConverter.IsISO8601DateTimeString("2016"));
+    }
+    [Fact(DisplayName = "should return false for invalid ISO 8601 date strings")]
+    public void ShouldReturnFalseForInvalidISO8601DateStrings()
+    {
+        Assert.False(DateTimeConverter.IsISO8601DateTimeString(null));
+        Assert.False(DateTimeConverter.IsISO8601DateTimeString(""));
+        Assert.False(DateTimeConverter.IsISO8601DateTimeString("qsdqsd"));
+        Assert.False(DateTimeConverter.IsISO8601DateTimeString("2016-02-01-"));
+        Assert.False(DateTimeConverter.IsISO8601DateTimeString("2021-01-01T00:00:00."));
     }
 }
--- src/CodeWF.Tools/Tools/DateTimeConverter.cs
+++ src/CodeWF.Tools/Tools/DateTimeConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 // ReSharper disable once CheckNamespace
 using CodeWF.Tools.Extensions;
@@ -7,35 +8,47 @@ namespace CodeWF.Tools;
 
 public class DateTimeConverter
 {
-    private const string ISO8601_REGEX =
-        @"^([+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24:?00)([.,]\d+(?!:))?)?(\17[0-5]\d([.,]\d+)?)?([zZ]|([+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$";
+    private static readonly Regex ISO8601_REGEX = new Regex(
+        @"^([+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24:?00)([.,]\d+(?!:))?)?(\17[0-5]\d([.,]\d+)?)?([zZ]|([+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$");
 
-    private const string ISO9075_REGEX =
-        @"^([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})(\.[0-9]{1,6})?(([+-])([0-9]{2}):([0-9]{2})|Z)?$";
+    private static readonly Regex ISO9075_REGEX =
+        new Regex(
+            @"^([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})(\.[0-9]{1,6})?(([+-])([0-9]{2}):([0-9]{2})|Z)?$");
 
-    private const string RFC3339_REGEX =
-        @"^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})(\.[0-9]{1,9})?(([+-])([0-9]{2}):([0-9]{2})|Z)$";
+    private static readonly Regex RFC3339_REGEX =
+        new Regex(
+            @"^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})(\.[0-9]{1,9})?(([+-])([0-9]{2}):([0-9]{2})|Z)$");
 
-    private const string RFC7231_REGEX =
-        @"^[A-Za-z]{3},\s[0-9]{2}\s[A-Za-z]{3}\s[0-9]{4}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\sGMT$";
+    private static readonly Regex RFC7231_REGEX =
+        new Regex(@"^[A-Za-z]{3},\s[0-9]{2}\s[A-Za-z]{3}\s[0-9]{4}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\sGMT$");
 
-    private const string EXCEL_FORMAT_REGEX = @"^-?\d+(\.\d+)?$";
+    private static readonly Regex EXCEL_FORMAT_REGEX = new Regex(@"^-?\d+(\.\d+)?$");
 
-    public static bool IsISO8601DateTimeString(string? date)
+    public static Func<string?, bool> CreateRegexMatcher(Regex regex)
     {
-        return !date.IsNullOrEmpty() && Regex.IsMatch(date!, ISO8601_REGEX);
+        return (date) => !string.IsNullOrWhiteSpace(date) && regex.IsMatch(date);
     }
+
+    public static Func<string?, bool> IsISO8601DateTimeString = CreateRegexMatcher(ISO8601_REGEX);
+    public static Func<string?, bool> IsISO9075DateString = CreateRegexMatcher(ISO9075_REGEX);
+    public static Func<string?, bool> IsRFC3339DateString = CreateRegexMatcher(RFC3339_REGEX);
+    public static Func<string?, bool> IsRFC7231DateString = CreateRegexMatcher(RFC7231_REGEX);
+    public static Func<string?, bool> IsUnixTimestamp = CreateRegexMatcher(new Regex(@"^[0-9]{1,10}$"));
+    public static Func<string?, bool> IsTimestamp = CreateRegexMatcher(new Regex(@"^[0-9]{1,13}$"));
+    public static Func<string?, bool> IsMongoObjectId = CreateRegexMatcher(new Regex(@"^[0-9a-fA-F]{24}$"));
+
+    public static Func<string?, bool> IsExcelFormat = CreateRegexMatcher(EXCEL_FORMAT_REGEX);
 
     public static bool IsUTCDateString(string? date)
     {
-        if (date.IsNullOrEmpty())
+        if (string.IsNullOrWhiteSpace(date))
         {
             return false;
         }
 
         try
         {
-            return DateTimeOffset.Parse(date).ToString() == date;
+            return DateTime.UtcNow.ToUniversalTime().ToString("r") == date;
         }
         catch
         {
@@ -43,13 +56,28 @@ public class DateTimeConverter
         }
     }
 
-    public static string DateToExcelFormat(DateTimeOffset date)
+    public static string DateToExcelFormat(DateTime date)
     {
-        return date.ToUnixTimeMilliseconds() / (1000 * 60 * 60 * 24) + 25569 + "";
+        TimeSpan timeSpan = date.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        double days = timeSpan.TotalDays;
+        return ((days + 25569)).ToString(CultureInfo.InvariantCulture);
     }
 
-    //public DateTimeOffset ExcelFormatToDate(string excelFormat)
-    //{
-    //    return DateTimeOffset.Parse((long.Parse(excelFormat) - 25569) * 86400 * 1000);
-    //}
+    public static DateTime ExcelFormatToDate(string excelFormat)
+    {
+        if (!double.TryParse(excelFormat, NumberStyles.Float, CultureInfo.InvariantCulture, out double days))
+        {
+            throw new ArgumentException("Invalid Excel format.", nameof(excelFormat));
+        }
+
+        double excelDays = days - 25569;
+        TimeSpan timeSpan = TimeSpan.FromDays(excelDays);
+        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Add(timeSpan);
+    }
+
+    public static DateTime ExcelFormatToDate(double excelDays)
+    {
+        TimeSpan timeSpan = TimeSpan.FromDays(excelDays - 25569);
+        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Add(timeSpan);
+    }
 }
