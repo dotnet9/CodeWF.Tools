57e7c7411bb8a6b4d39dc8c3a27041cb99f3790d
Henry <1012434131@qq.com>
2024-12-03 11:07:16 +0800

    add ThisAssembly;

--- src/CodeWF.Tools.AvaloniaDemo/ViewModels/MainViewModel.cs
+++ src/CodeWF.Tools.AvaloniaDemo/ViewModels/MainViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace CodeWF.Tools.AvaloniaDemo.ViewModels;
+﻿using CodeWF.Tools.Extensions;
+
+namespace CodeWF.Tools.AvaloniaDemo.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+
+    public string? AssemblyInfo { get; set; }
+
+    public MainViewModel()
+    {
+        AssemblyInfo = $"{nameof(ThisAssembly.Title)}: {ThisAssembly.Title}\r\n" +
+                       $"{nameof(ThisAssembly.Description)}: {ThisAssembly.Description}\r\n" +
+                       $"{nameof(ThisAssembly.Company)}: {ThisAssembly.Company}\r\n" +
+                       $"{nameof(ThisAssembly.Product)}: {ThisAssembly.Product}\r\n" +
+                       $"{nameof(ThisAssembly.Version)}: {ThisAssembly.Version}\r\n" +
+                       $"{nameof(ThisAssembly.InformationalVersion)}: {ThisAssembly.InformationalVersion}\r\n" +
+                       $"{nameof(ThisAssembly.FileVersion)}: {ThisAssembly.FileVersion}\r\n" +
+                       $"{nameof(ThisAssembly.Copyright)}: {ThisAssembly.Copyright}\r\n" +
+                       $"{nameof(ThisAssembly.CompileTime)}: {ThisAssembly.CompileTime:yyyy-MM-dd HH:mm:ss}\r\n";
+    }
 }
