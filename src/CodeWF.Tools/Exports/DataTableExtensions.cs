112d16b90a8132d5a43e9b5592ee1c253c156bfa
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-12-24 21:18:55 +0800

    add DataTableExtensions;

---
+++ src/CodeWF.Tools/Exports/DataTableExtensions.cs
@@ -0,0 +1,83 @@
+﻿using CsvHelper;
+using MiniExcelLibs;
+using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CodeWF.Tools.Exports;
+
+public static class DataTableExtensions
+{
+    public static bool Export(this DataTable dataTable, string saveFilePath, out string errorMsg,
+        bool containColumnHeader = true)
+    {
+        if (saveFilePath.ToLower().EndsWith(".csv"))
+        {
+            return dataTable.ExportToCsv(saveFilePath, out errorMsg, containColumnHeader);
+        }
+
+        if (saveFilePath.ToLower().EndsWith("*.xlsx"))
+        {
+            return dataTable.ExportToXlsx(saveFilePath, out errorMsg, containColumnHeader);
+        }
+
+        throw new Exception("Currently only supports .csv and .xlsx file exports");
+    }
+
+
+    public static bool ExportToCsv(this DataTable dataTable, string saveFilePath, out string errorMsg,
+        bool containColumnHeader = true)
+    {
+        errorMsg = "";
+        try
+        {
+            using var writer = new StreamWriter(saveFilePath, false, Encoding.Default);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            if (containColumnHeader)
+            {
+                foreach (DataColumn dc in dataTable.Columns)
+                {
+                    csv.WriteField(dc.ColumnName);
+                }
+
+                csv.NextRecord();
+            }
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                foreach (DataColumn dc in dataTable.Columns)
+                {
+                    csv.WriteField(dr[dc]);
+                }
+
+                csv.NextRecord();
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            errorMsg = ex.Message;
+            return false;
+        }
+    }
+
+    public static bool ExportToXlsx(this DataTable dataTable, string saveFilePath, out string errorMsg,
+        bool containColumnHeader = true)
+    {
+        errorMsg = "";
+        try
+        {
+            MiniExcel.SaveAs(saveFilePath, dataTable);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            errorMsg = ex.Message;
+            return false;
+        }
+    }
+}
