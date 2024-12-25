1354fd07ac18ae8a2259d3ee5c81b0abac3a5397
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-12-25 21:56:34 +0800

    add ImageHelper;

---
+++ src/CodeWF.Tools.Test/ImageHelperTest.cs
@@ -0,0 +1,43 @@
+﻿using CodeWF.Tools.FileExtensions;
+using CodeWF.Tools.Helpers;
+
+namespace CodeWF.Tools.Test;
+
+public class ImageHelperTest
+{
+    [Fact]
+    public async Task Test_MergeGenerateIcon_Success()
+    {
+        GetImageInfo(out var sourceImage, out var destImage);
+        await ImageHelper.MergeGenerateIcon(sourceImage, destImage, [24, 32]);
+
+        Assert.True(File.Exists(destImage));
+
+        FileHelper.DeleteFileIfExist(destImage);
+    }
+
+    [Fact]
+    public async Task Test_SeparateGenerateIcon_Success()
+    {
+        GetImageInfo(out var sourceImage, out var destImage);
+        await ImageHelper.SeparateGenerateIcon(sourceImage, destImage, [24, 32]);
+
+        var destDir = Path.GetDirectoryName(destImage);
+        var iconFiles = Directory.GetFiles(destDir, "*.ico");
+        Assert.True(iconFiles.Length > 0);
+
+        foreach (var iconFile in iconFiles)
+        {
+            FileHelper.DeleteFileIfExist(iconFile);
+        }
+    }
+
+    private void GetImageInfo(out string sourceImage, out string destImage)
+    {
+        sourceImage = "../../logo.png";
+        Assert.True(File.Exists(sourceImage));
+
+        destImage = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logo.ico");
+        Assert.False(File.Exists(destImage));
+    }
+}
---
+++ src/CodeWF.Tools/Helpers/ImageHelper.cs
@@ -0,0 +1,40 @@
+﻿using ImageMagick;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeWF.Tools.Helpers;
+
+public static class ImageHelper
+{
+    public static async Task MergeGenerateIcon(string sourceImagePath, string destIconPath, uint[] sizes)
+    {
+        var baseImage = new MagickImage(sourceImagePath);
+        var collection = new MagickImageCollection();
+
+        foreach (var size in sizes)
+        {
+            var resizedImage = baseImage.Clone();
+            resizedImage.Resize(size, size);
+            collection.Add(resizedImage);
+        }
+
+        await collection.WriteAsync(destIconPath);
+    }
+
+    public static async Task SeparateGenerateIcon(string sourceImagePath, string destIconPath, uint[] sizes)
+    {
+        var folder = Path.GetDirectoryName(destIconPath);
+        var fileName = Path.GetFileNameWithoutExtension(destIconPath);
+
+        var baseImage = new MagickImage(sourceImagePath);
+
+        foreach (var size in sizes)
+        {
+            var resizedImage = baseImage.Clone();
+            resizedImage.Resize(size, size);
+
+            var savePath = Path.Combine(folder, $"{fileName}.{size}x{size}.ico");
+            await resizedImage.WriteAsync(savePath);
+        }
+    }
+}
