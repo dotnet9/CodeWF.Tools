76af3096b8c1c5ff70c375e72d46e09e815cb7ee
Henry <1012434131@qq.com>
2025-02-06 18:11:02 +0800

    Update JsonExtensionsTest.cs

--- src/CodeWF.Tools.Test/JsonExtensionsTest.cs
+++ src/CodeWF.Tools.Test/JsonExtensionsTest.cs
@@ -1,4 +1,6 @@
-﻿namespace CodeWF.Tools.Test;
+﻿using System.Collections;
+
+namespace CodeWF.Tools.Test;
 
 public class JsonExtensionsTest
 {
@@ -20,5 +22,21 @@ public class JsonExtensionsTest
 
         Assert.True(obj.ToJson(out var jsonString, out var errorMsg));
         Assert.True(jsonString.JsonToYaml(out var yamlString, out errorMsg));
+    }
+
+    [Fact]
+    public void Test_Hashtable2Json_Success()
+    {
+        Hashtable table1 = new();
+        table1[1] = "wq";
+        table1["you"] = 2;
+        table1[2] = "and me";
+
+        table1.ToJson(out var json, out var errorMsg);
+
+        var convertResult = json.FromJson(out Hashtable table2, out errorMsg);
+
+        Assert.True(convertResult);
+        Assert.Equal(table1.Count, table2.Count);
     }
 }
