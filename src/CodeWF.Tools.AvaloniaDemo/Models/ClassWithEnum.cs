801fd4055f2bd8e73c6b35399304e3f3e3988bcc
Henry <1012434131@qq.com>
2025-02-12 13:09:46 +0800

    update demo;

--- src/CodeWF.Tools.AvaloniaDemo/Models/ClassWithEnum.cs
+++ src/CodeWF.Tools.AvaloniaDemo/Models/ClassWithEnum.cs
@@ -1,19 +1,18 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace CodeWF.Tools.AvaloniaDemo.Models
+namespace CodeWF.Tools.AvaloniaDemo.Models;
+
+public class ClassWithEnum
 {
-    public class ClassWithEnum
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
+    public int Id { get; set; }
+    public string? Name { get; set; }
 
-        public TestEnum Type { get; set; }
-    }
+    public TestEnum Type { get; set; }
+}
 
-    [JsonConverter(typeof(JsonStringEnumConverter<TestEnum>))]
-    public enum TestEnum
-    {
-        Light,
-        Dark
-    }
+[JsonConverter(typeof(JsonStringEnumConverter<TestEnum>))]
+public enum TestEnum
+{
+    Light,
+    Dark
 }
