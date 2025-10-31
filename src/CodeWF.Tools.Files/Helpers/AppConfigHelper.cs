f12280673c6ef27d1926b46c04e067dc6160fe8e
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-10-31 18:05:56 +0800

    add GetOrAdd;

--- src/CodeWF.Tools.Files/Helpers/AppConfigHelper.cs
+++ src/CodeWF.Tools.Files/Helpers/AppConfigHelper.cs
@@ -38,26 +38,38 @@ public static class AppConfigHelper
         }
     }
 
-    public static bool TryGetOrSet<T>(string name,out T? value, T? defaultValue = default)
-    {
-        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-        if (config.TryGet(name, out value))
-        {
-            return true;
-        }
-        else
-        {
-            config.SetOrAdd(name, defaultValue);
-            value = defaultValue;
-            return false;
-        }
-    }
-
-
     public static bool TryGet<T>(string name, out T? value)
     {
         var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
         return config.TryGet(name, out value);
+    }
+
+    public static bool TryGetOrSet<T>(this Configuration config, string name, out T? value, T? defaultValue = default)
+    {
+        try
+        {
+            if (config.TryGet(name, out value))
+            {
+                return true;
+            }
+            else
+            {
+                config.SetOrAdd(name, defaultValue);
+                value = defaultValue;
+                return false;
+            }
+        }
+        catch
+        {
+            value = default;
+            return false;
+        }
+    }
+
+    public static bool TryGetOrSet<T>(string name, out T? value, T? defaultValue = default)
+    {
+        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        return config.TryGetOrSet(name, out value, defaultValue);
     }
 
     public static void Set<T>(this Configuration config, string name, T value)
@@ -90,5 +102,17 @@ public static class AppConfigHelper
     {
         var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None); 
         config.SetOrAdd(name, value);
+    }
+    public static T GetOrAdd<T>(this Configuration config, string name, T? defaultValue)
+    {
+        var value = defaultValue;
+        config.TryGetOrSet(name, out value, defaultValue);
+        return value;
+    }
+
+    public static T GetOrAdd<T>(string name, T? defaultValue)
+    {
+        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        return config.GetOrAdd(name, defaultValue);
     }
 }
