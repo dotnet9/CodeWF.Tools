a322a2327807fe3f96e26d356e228e1da7692c49
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-02-07 13:55:39 +0800

    Create FileExtension.cs

---
+++ src/CodeWF.Tools.Files/FileExtensions/FileExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWF.Tools.Files.FileExtensions;
+
+public static class FileExtension
+{
+    public static bool IsPathInUpperTwoLevelsOfCurrentDir(string targetPath)
+    {
+        string currentDir = Path.GetFullPath(Directory.GetCurrentDirectory());
+        currentDir = AddTrailingDirectorySeparator(currentDir);
+        string upperTwoDir = Path.GetFullPath(Path.Combine(currentDir, "..", ".."));
+        upperTwoDir = AddTrailingDirectorySeparator(upperTwoDir);
+        string normalizedTarget = Path.GetFullPath(targetPath);
+        if (File.Exists(normalizedTarget))
+        {
+            normalizedTarget = Path.GetDirectoryName(normalizedTarget)!;
+        }
+        normalizedTarget = AddTrailingDirectorySeparator(normalizedTarget);
+
+
+        return normalizedTarget.StartsWith(upperTwoDir, StringComparison.Ordinal);
+    }
+
+    private static string AddTrailingDirectorySeparator(string path)
+    {
+        if (string.IsNullOrEmpty(path)) return path;
+        if (!Path.EndsInDirectorySeparator(path))
+        {
+            path += Path.DirectorySeparatorChar;
+        }
+        return path;
+    }
+}
